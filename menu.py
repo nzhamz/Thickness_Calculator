00fffe9ae0c629742d18bde84c981af029e41ac7
nzhamz <64738228+nzhamz@users.noreply.github.com>
2024-04-27 21:33:24 +0300

    menu.py

--- menu.py
+++ menu.py
@@ -7,53 +7,56 @@ Created on Tue Apr 23 20:55:17 2024
 """
 
 import streamlit as st
-import math
 import requests
 
-
-import streamlit as st
-
+# Create a text input widget for the image URL
+filenamei1= 'NuraMark2'
+model_url0 = 'https://huggingface.co/datasets/Nzham/CRY/resolve/main/NuraMark2.png?download=true'
 
 def open_link(url):
-    """Opens a link in a new tab using JavaScript."""
-    st.write(f'<a href="{url}" target="_blank">Click here for {url.split("/")[-1]}</a>', unsafe_allow_html=True)
+  """Opens a link in a new tab using JavaScript."""
+  st.write(f'<a href="{url}" target="_blank">Click here for {url.split("/")[-1]}</a>', unsafe_allow_html=True)
+
+# Check if the URL is not empty
+if model_url0:
+    # Send a GET request to the URL and get the response
+    response = requests.get(model_url0)
+    # Display the image from the response
+    st.image(response.content, caption="CMC", use_column_width=True)
+    
+    
+# Title for the Streamlit app
+st.title('Welcome to Corrosion Management Calculator (CMC) : ')
+
+st.write('')
 
 
-def tank_retired_thickness_calculator():
-    # Add your content for calculating tank retired thickness here
-    st.subheader("Tank Retired Thickness Calculator")
-    # Add functionalities like input fields, calculations, and result display
-    pass
+# Button for CYP
+if st.button('Pipe Retirement Thickness Calculator'):
+    # Open the link in a new tab
+    open_link('https://thicknesscalculator-cifmkkwkbnuzztnoxsaruv.streamlit.app/')
+
+st.write('')
 
 
-def pipe_retired_thickness_calculator():
-    # Add your content for calculating pipe retired thickness here
-    st.subheader("Pipe Retired Thickness Calculator")
-    # Add functionalities like input fields, calculations, and result display
-    pass
+# Button for TASI
+if st.button('Tank Retirement Thickness Calculator'):
+    # Open the link in a new tab
+    st.write('Comming SOOOOOON!!!')
+    #open_link('https://tasi-nuramark.onrender.com/')
+
+st.write('')
 
 
-def cathodic_protection_calculator():
-    # Add your content for calculating cathodic protection total anodes here
-    st.subheader("Calculating Cathodic Protection Total Anodes")
-    # Add functionalities like input fields, calculations, and result display
-    pass
+# Button for TASI
+if st.button('Cathodic Protection Total Anodes Calculator'):
+    st.write('Comming SOOOOOON!!!')
 
 
-st.title("Static Equipment Calculator Suite (SECU) ")
-st.header("Web-based Corrosion Management Calculator")
+st.write('')
 
+imo= ':rocket:'* 9
 
-# Use function calls for button clicks
-if st.button("Tank Retired Thickness Calculator"):
-    tank_retired_thickness_calculator()
+st.title(imo)
 
-elif st.button("Pipe Retired Thickness Calculator"):
-    pipe_retired_thickness_calculator()
-
-elif st.button("Calculating Cathodic Protection Total Anodes"):
-    cathodic_protection_calculator()
-
-else:
-    st.write("Please select a calculation tool.")
 
